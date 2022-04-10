3c0f9d298eb381fb5cf198339f61ea57bf43eaa9
Margarita Novokh <r28611@gmail.com>
2022-04-11 01:11:47 +0200

    created first ever screen with SwiftUI

--- Find Art/ContentView.swift
+++ Find Art/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var login = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, User!")
+                .padding(.top, 32)
+                .font(.largeTitle)
+            HStack {
+                Text("Login:")
+                Spacer()
+                TextField("Enter login", text: $login) .frame(maxWidth: 150) .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            HStack { Text("Password:")
+                Spacer()
+                SecureField("Enter password", text: $password)
+                    .frame(maxWidth: 150)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            Button(action: self.onLoginPressed) {
+                Label("Sign in", systemImage: "arrow.right.circle")
+            }.padding(.top, 50)
+                .padding(.bottom, 20)
+                .disabled(login.isEmpty || password.isEmpty)
+            Spacer()
+        }.frame(maxWidth: 250)
+    }
+    
+    private func onLoginPressed() {
+        
     }
 }
 
