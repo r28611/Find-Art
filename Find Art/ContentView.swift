65cd40df480fedd9915fe61a2298ff55582e4d58
Margarita Novokh <r28611@gmail.com>
2022-04-10 23:14:16 +0200

    Initial Commit

---
+++ Find Art/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Find Art
+//
+//  Created by Margarita Novokhatskaia on 10/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Find Art/Find_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_ArtApp.swift
+//  Find Art
+//
+//  Created by Margarita Novokhatskaia on 10/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Find_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
